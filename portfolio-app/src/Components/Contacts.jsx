b814dae1313086fa23295342d4b9ca4dd8ea7145
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-03-22 23:42:16 +0300

    modify skills details

--- portfolio-app/src/Components/Contacts.jsx
+++ portfolio-app/src/Components/Contacts.jsx
@@ -7,9 +7,9 @@ function Contacts() {
     <div className='contact'>
         
         <div className='h-[100vh] flex justify-center items-center '>
-            <div className='contact-form p-10 mt-40 rounded-4xl'>
+            <div className='contact-form p-10 mt-40 rounded-4xl max-[600px]:w-[80vw] flex flex-col justify-center items-center'>
                  <h2 className='inline-block mb-5 pb-2 font-poppins text-xl text-[#6e65f6] font-bold border-b-2'>Get in touch</h2>
-                 <form action="https://api.web3forms.com/submit" method="POST"  className=' project form flex flex-col w-86 p-3 rounded-2xl'>
+                 <form action="https://api.web3forms.com/submit" method="POST"  className=' form flex flex-col w-86 justify-center items-center max-[600px]:w-50 rounded-2xl'>
                     <input type='hidden' name='access_key' value='779729dc-1f97-4813-9ef6-5009a95ad9fd'/>
                     <input type='text' name='name' placeholder='Your Name' required />
                     <input type='email' name='email' placeholder='Your Email' required/>
